a5fd7a79da04cb250d38782e2ffc18f2c41fffd1
M-Nikhita <nikhitaalexander@gmail.com>
2026-04-28 23:29:10 +0530

    Strengthen password validation: 8+ chars uppercase number special char

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,8 +15,10 @@ const registerValidation = [
   body('name').trim().notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Valid email is required'),
   body('password')
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters'),
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[0-9]/).withMessage('Password must contain at least one number')
+    .matches(/[!@#$%^&*(),.?":{}|<>_\-]/).withMessage('Password must contain at least one special character'),
 ];
 
 const loginValidation = [
